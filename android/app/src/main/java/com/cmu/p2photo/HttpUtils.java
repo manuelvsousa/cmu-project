d1f56f82f4c2020b113859998e150b98900641c2
Manuel Sousa <manuelsousamvs@gmail.com>
2019-04-22 15:44:30 +0100

    Android app is now registering and loging users

---
+++ android/app/src/main/java/com/cmu/p2photo/HttpUtils.java
@@ -0,0 +1,37 @@
+package com.cmu.p2photo;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.message.BasicHeader;
+
+public class HttpUtils {
+    private static final String BASE_URL = "http://192.168.1.72:5000/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        Header[] headers = {
+                new BasicHeader("Content-type", "application/json")
+        };
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
