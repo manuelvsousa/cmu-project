eaaaf0f739a4ce4674c93ed852fd54421636f299
Manuel Sousa <manuelsousamvs@gmail.com>
2019-05-04 23:55:50 +0100

    Add upload photo feature

--- android/app/src/main/java/com/cmu/p2photo/ViewAlbum.java
+++ android/app/src/main/java/com/cmu/p2photo/ViewAlbum.java
@@ -1,13 +1,53 @@
 package com.cmu.p2photo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.cmu.p2photo.drive.CreateFileTask;
+import com.cmu.p2photo.drive.DropboxClientFactory;
+import com.cmu.p2photo.drive.UploadFileTask;
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 
 public class ViewAlbum extends AppCompatActivity {
@@ -17,9 +57,13 @@ public class ViewAlbum extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_album);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1337);
+        }
 
-
-        String album = getIntent().getStringExtra("album");
+        final String album = getIntent().getStringExtra("album");
         TextView tv = findViewById(R.id.albumTextView);
         tv.setText("Album: " + album);
 
@@ -29,18 +73,144 @@ public class ViewAlbum extends AppCompatActivity {
         addPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(ViewAlbum.this, CreateAlbum.class);
-//                startActivity(intent);
-
-
-
-
-
-
-
+//
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),1);
 
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1337) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("OLA", "heehehhehehhehhe");
+            } else {
+                // User refused to grant permission.
+            }
+        }
+    }
+
+    //                    ImageView asd = findViewById(R.id.imageView);
+//                    asd.setImageURI(selectedImageUri);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (resultCode == RESULT_OK) {
+                if (requestCode == 1) {
+                    Uri selectedImageUri = data.getData();
+                    // Get the path from the Uri
+                    final String path = getRealPathFromURI_API19(getApplicationContext(),selectedImageUri);
+                    if (path != null) {
+                        File f = new File(path);
+                        Log.d("CARALHO", path);
+                        Log.d("NAO PODE", path);
+                        Log.d("FODASSE", f.getName());
+                        Log.d("FODASSE", f.getAbsolutePath());
+                        selectedImageUri = Uri.fromFile(f);
+                    }
+
+
+
+
+                    final String album = getIntent().getStringExtra("album");
+                    // Set the image in ImageView
+                    Log.d("FODASSE", selectedImageUri.toString());
+                    Log.d("FODASSE", selectedImageUri.toString() + "caralho");
+                    new UploadFileTask(getApplicationContext(), DropboxClientFactory.getClient(), new UploadFileTask.Callback() {
+                        @Override
+                        public void onUploadComplete(String result) {
+                            Log.d("FODASSE", result);
+
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+                            Log.e("FODASSE", "Failed to upload file.", e);
+                        }
+                    }).execute(path,album);
+
+//
+                }
+            }
+        } catch (Exception e) {
+            Log.e("FileSelectorActivity", "File select error", e);
+        }
+    }
+
+    private String getPathFromURI(Uri uri) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        inputStream.close();
+        return stringBuilder.toString();
+    }
+
+    @SuppressLint("NewApi")
+    public static String getRealPathFromURI_API19(Context context, Uri uri){
+
+
+
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = { MediaStore.Images.Media.DATA };
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, new String[]{ id }, null);
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+
+    @SuppressLint("NewApi")
+    public static String getRealPathFromURI_API11to18(Context context, Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        String result = null;
+
+        CursorLoader cursorLoader = new CursorLoader(
+                context,
+                contentUri, proj, null, null, null);
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        if(cursor != null){
+            int column_index =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            result = cursor.getString(column_index);
+        }
+        return result;
+    }
+
+    public static String getRealPathFromURI_BelowAPI11(Context context, Uri contentUri){
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+        int column_index
+                = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
--- android/app/src/main/java/com/cmu/p2photo/drive/UploadFileTask.java
+++ android/app/src/main/java/com/cmu/p2photo/drive/UploadFileTask.java
@@ -4,38 +4,45 @@ package com.cmu.p2photo.drive;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.CreateFolderErrorException;
 import com.dropbox.core.v2.files.FileMetadata;
 import com.dropbox.core.v2.files.WriteMode;
+import com.dropbox.core.v2.sharing.SharedLinkMetadata;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 
-class UploadFileTask extends AsyncTask<String, Void, FileMetadata> {
+public class UploadFileTask extends AsyncTask<String, Void, String> {
+
 
     private final Context mContext;
     private final DbxClientV2 mDbxClient;
     private final Callback mCallback;
     private Exception mException;
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
 
     public interface Callback {
-        void onUploadComplete(FileMetadata result);
+        void onUploadComplete(String result);
         void onError(Exception e);
     }
 
-    UploadFileTask(Context context, DbxClientV2 dbxClient, Callback callback) {
+    public UploadFileTask(Context context, DbxClientV2 dbxClient, Callback callback) {
         mContext = context;
         mDbxClient = dbxClient;
         mCallback = callback;
     }
 
     @Override
-    protected void onPostExecute(FileMetadata result) {
+    protected void onPostExecute(String result) {
         super.onPostExecute(result);
         if (mException != null) {
             mCallback.onError(mException);
@@ -46,26 +53,47 @@ class UploadFileTask extends AsyncTask<String, Void, FileMetadata> {
         }
     }
 
+    public static String randomAlphaNumeric(int count) {
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
+
     @Override
-    protected FileMetadata doInBackground(String... params) {
-        String localUri = params[0];
-        File localFile = UriHelpers.getFileForUri(mContext, Uri.parse(localUri));
+    protected String doInBackground(String... params) {
+        String randomString = randomAlphaNumeric(32);
+        String[]  originalSplitedFileName = params[0].split("/");
+        String fileName =  "/" + params[1] + "/" + randomString + originalSplitedFileName[originalSplitedFileName.length - 1];
+        try {
+            try {
+                File asd = new File(params[0]);
 
-        if (localFile != null) {
-            String remoteFolderPath = params[1];
-
-            // Note - this is not ensuring the name is a valid dropbox file name
-            String remoteFileName = localFile.getName();
-
-            try (InputStream inputStream = new FileInputStream(localFile)) {
-                return mDbxClient.files().uploadBuilder(remoteFolderPath + "/" + remoteFileName)
+                Log.d("FODASSE",asd.toString());
+                Log.d("FODASSE",fileName);
+                InputStream inputStream = new FileInputStream(asd);
+                mDbxClient.files().uploadBuilder(fileName)
                         .withMode(WriteMode.OVERWRITE)
                         .uploadAndFinish(inputStream);
+
             } catch (DbxException | IOException e) {
                 mException = e;
             }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-
+        try{
+            SharedLinkMetadata meta =  mDbxClient.sharing().createSharedLinkWithSettings(fileName);
+            String url = meta.getUrl();
+            url = url.split("\\?")[0];
+            url = url + "\\?raw=1";
+            Log.d("FODASSE",url);
+            return url;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 }
