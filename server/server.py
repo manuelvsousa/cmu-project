4238aac91df0bb5678e29b575827fe58d0825e2d
Manuel Sousa <manuelsousamvs@gmail.com>
2019-05-09 23:42:41 +0100

    Fix typo in previous fix

--- server/server.py
+++ server/server.py
@@ -201,11 +201,11 @@ def addUserAlbum():
         resp = jsonify(success=False, message="You are already in this album")
         return make_response(resp, 400)
 
-    if db.User.query.filter_by(username=user).count() >= 1:
+    if db.User.query.filter_by(username=user).count() != 1:
         resp = jsonify(success=False, message=user + " does not exist")
         return make_response(resp, 404)
 
-    if db.Album.query.filter_by(username=user, album=albumName).count() != 1:
+    if db.Album.query.filter_by(username=user, album=albumName).count() >= 1:
         resp = jsonify(success=False, message=user +
                        " already in album: " + albumName)
         return make_response(resp, 400)
